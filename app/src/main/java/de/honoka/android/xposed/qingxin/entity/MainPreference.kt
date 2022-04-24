dd2a0387a0b6a6df762303b8bfd4b2eb9a6a99e7
Kosaka Bun <kosaka-bun@qq.com>
2022-04-24 18:45:48 +0800

    修正Kotlin引用lombok getter时的编译错误

--- app/src/main/java/de/honoka/android/xposed/qingxin/entity/MainPreference.kt
+++ app/src/main/java/de/honoka/android/xposed/qingxin/entity/MainPreference.kt
@@ -1,61 +1,62 @@
-package de.honoka.android.xposed.qingxin.entity;
+package de.honoka.android.xposed.qingxin.entity
 
-import lombok.Data;
-import lombok.experimental.Accessors;
-
-@Data
-@Accessors(chain = true)
-public class MainPreference {
+class MainPreference {
 
     /**
      * 屏蔽所有首页推广
      */
-    private Boolean blockAllMainPagePublicity;
+    var blockAllMainPagePublicity: Boolean? = null
 
     /**
      * 屏蔽所有热搜
      */
-    private Boolean blockAllHotSearchWords;
+    var blockAllHotSearchWords: Boolean? = null
 
     /**
      * 还原所有竖屏视频
      */
-    private Boolean convertAllVerticalAv;
+    var convertAllVerticalAv: Boolean? = null
 
     /**
      * 是否在框架控制台输出拦截日志
      */
-    private Boolean showBlockLog;
+    var showBlockLog: Boolean? = null
 
     /**
      * 是否在拦截时发出气泡信息，以显示拦截条数等信息
      */
-    private Boolean toastOnBlock;
+    var toastOnBlock: Boolean? = null
 
     /**
      * 调试模式（会在控制台输出很多调试信息）
      */
-    private Boolean testMode;
+    var testMode: Boolean? = null
 
     /**
      * 屏蔽所有推荐话题
      */
-    private Boolean blockRecommendedTopics;
+    var blockRecommendedTopics: Boolean? = null
 
     /**
      * 禁用播放器长按加速
      */
-    private Boolean disablePlayerLongPress;
+    var disablePlayerLongPress: Boolean? = null
 
-    public static MainPreference getDefaultPreference() {
-        return new MainPreference()
-                .setBlockAllMainPagePublicity(false)
-                .setBlockAllHotSearchWords(false)
-                .setConvertAllVerticalAv(false)
-                .setShowBlockLog(false)
-                .setToastOnBlock(false)
-                .setTestMode(true)
-                .setBlockRecommendedTopics(false)
-                .setDisablePlayerLongPress(false);
+    companion object {
+
+        @JvmStatic
+        val defaultPreference: MainPreference
+            get() {
+                val mainPreference = MainPreference()
+                mainPreference.blockAllMainPagePublicity = false
+                mainPreference.blockAllHotSearchWords = false
+                mainPreference.convertAllVerticalAv = false
+                mainPreference.showBlockLog = false
+                mainPreference.toastOnBlock = false
+                mainPreference.testMode = true
+                mainPreference.blockRecommendedTopics = false
+                mainPreference.disablePlayerLongPress = false
+                return mainPreference
+            }
     }
 }
